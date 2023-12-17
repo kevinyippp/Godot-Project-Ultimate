d12cb8a89cbac20f7fed0ea66799a564b8b36312
Kevin Yip <this_is_not_real_email@gmail.com>
2023-12-17 00:03:52 -0800

    update

--- Game Scripts/Player.cs
+++ Game Scripts/Player.cs
@@ -23,12 +23,22 @@ public partial class Player : CharacterBody2D
 
 	public override void _Process(double delta)
 	{	
+		RotateToMouse();
+
 		playerDirection = Input.GetVector("Left", "Right", "Up", "Down");
 
-		Velocity = playerDirection * speed;
-	
-		GD.Print( MoveAndCollide( Velocity * (float)delta) );
+		Velocity = playerDirection * speed * (float)delta;
 
-		LookAt(GetGlobalMousePosition() ); //rotate player to mouse direction
+		MoveAndCollide(Velocity);
+
+		GD.Print($"global: {this.GetGlobalMousePosition()}, local: {this.GetLocalMousePosition()}");
+		
+	}
+
+	void RotateToMouse()
+	{
+		Vector2 mouseDirection = this.GetGlobalMousePosition() - this.Position;
+
+		this.Rotation = Mathf.Atan2(mouseDirection.Y, mouseDirection.X);
 	}
 }
